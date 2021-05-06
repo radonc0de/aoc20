5836c0a2c2d15e702f27bcc47c5be2c65eaa0b1c
keith L <keithl@radontech.xyz>
2021-05-06 01:55:39 -0400

    cleaned up

--- day_1/target/debug/day_1.d
+++
@@ -1,1 +0,0 @@
-/home/keith/Documents/Code/AoC20/day_1/target/debug/day_1: /home/keith/Documents/Code/AoC20/day_1/src/main.rs
--- day_1/target/debug/deps/day_1-88217881dca73214.d
+++
@@ -1,5 +0,0 @@
-/home/keith/Documents/Code/AoC20/day_1/target/debug/deps/day_1-88217881dca73214: src/main.rs
-
-/home/keith/Documents/Code/AoC20/day_1/target/debug/deps/day_1-88217881dca73214.d: src/main.rs
-
-src/main.rs:
